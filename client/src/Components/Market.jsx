0372199d710904431e27fb1d5e18f863dde62415
Nina Poliak-Karatkevich <ninapoliakkarat@gmail.com>
2023-02-16 03:25:02 -0500

    formating market table data

--- client/src/Components/Market.jsx
+++ client/src/Components/Market.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import apiServices from "../Context/apiServices";
-import CoinTrending from "./CoinTrending";
-import {Table} from "@mui/material";
 
 function Market() {
-    const { response }= apiServices('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false');
-    function tablePages() {
-        
+    const {response} = apiServices('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+    const numberFormat = (value) => {
+        return new Intl.NumberFormat('en-IN', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 6,
+        }).format(value);
     }
-    console.log(response)
     return (
-        <div className="bg-slate-800 rounded">
-            <h1 className="text-slate-300 font-bold p-3">Trending:</h1>
+        <div className="bg-slate-800 rounded m-2">
                 <table className="min-w-full divide-y divide-gray-200">
                     <thead>
                     <tr>
@@ -24,30 +25,31 @@ function Market() {
                         <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Trend</th>
                     </tr>
                     </thead>
-                    <tbody className="divide-y divide-gray-200">
+                    <tbody className="divide-y divide-gray-200" >
                     {response && response.map(elem =>
-                        <tr key={elem.id}>
-                            <td className="flex flex-row text-left items-center px-6 py-4 whitespace-nowrap h-1/3 max-h-20">
-                                {elem.id}
+                        <tr key={elem.id} className="hover:bg-gray-600" onClick={() => console.log(elem.id)}>
+                            <td className="flex flex-row text-left items-center px-6 py-4 whitespace-nowrap max-h-20">
+                                {elem.name}
                                 <img className="scale-[.25] ml-auto" src={elem.image} alt={elem.id}/>
                             </td>
                             <td className="text-left px-6 py-4 pl-0 whitespace-nowrap m-0">
-                                {elem.current_price}
+                                {numberFormat(elem.current_price)}
                             </td>
                             <td className="text-left px-6 py-4 whitespace-nowrap m-0">
-                                {elem.market_cap}
+                                {numberFormat(elem.market_cap)}
                             </td>
                             <td className="text-left px-6 py-4 whitespace-nowrap m-0">
-                                {elem.total_volume}
+                                {numberFormat(elem.total_volume)}
                             </td>
                             <td className="text-left px-6 py-4 whitespace-nowrap m-0">
-                                {elem.price_change_24h}
+                                {numberFormat(elem.price_change_24h)}
                             </td>
                             <td className="text-left px-6 py-4 whitespace-nowrap m-0">
-                                {elem.price_change_percentage_24h}
+                                {Number(elem.price_change_percentage_24h).toFixed(2)}%
                             </td>
                             <td className="text-left px-6 py-4 whitespace-nowrap m-0">
                              {/*Check precent change if pos add a pos trend if neg add neg trend*/}
+                                {Number(elem.price_change_percentage_24h) > 0 ? <p className="text-green-800">Pos Trend</p> : <p className="text-red-800">Neg Trend</p>}
                             </td>
                         </tr>
 
